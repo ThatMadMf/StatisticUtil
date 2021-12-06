9e92150ae591d7f7b7ca7f45f0a620dbdcaf8f4c
Max Onoshko <maxdarksiders@gmail.com>
2021-12-06 17:30:50 +0200

    Implement resolving data consistency

--- src/solver.js
+++ src/solver.js
@@ -3,6 +3,7 @@ class Solver {
         this.scores = scores;
         this.quantitativeAssessment();
         this.rankAnalysis();
+        this.dataConsistency();
     }
 
     quantitativeAssessment() {
@@ -24,7 +25,7 @@ class Solver {
             return {
                 id: i.id,
                 values: i.scores.map(s => s.score).map(n => getRank(+n))
-            }
+            };
         });
         this.ranksSum = [];
         for (let i = 0; i < Math.min(...this.itemRanks.map(item => item.values.length)); i++) {
@@ -36,6 +37,42 @@ class Solver {
         this.resultRank = this.ranksSum.map(s => [...new Set(this.ranksSum)].sort((a, b) => a - b).indexOf(s) + 1);
     }
 
+    dataConsistency() {
+        this.Var = [];
+        for (let i = 1; i <= Math.min(...this.scores.map(item => item.scores.length)); i++) {
+            this.Var.push(
+                Math.max(...this.scores.map(item => item.scores.find(score => score.id === i).score)) -
+                Math.min(...this.scores.map(item => item.scores.find(score => score.id === i).score))
+            );
+        }
+
+        this.d = [];
+        for(let i = 1; i <= Math.min(...this.scores.map(item => item.scores.length)); i++) {
+            this.d.push(
+                this.scores
+                    .map(item => item.scores.find(score => score.id === i).score)
+                    .map(score => Math.pow(score - this.x[i - 1], 2))
+                    .reduce((a, b) => a + b) / this.scores.length
+            );
+        }
+
+        this.sigma = this.d.map(d => Math.sqrt(d));
+        this.v = [];
+
+        for(let i = 0; i < Math.min(...this.scores.map(item => item.scores.length)); i++) {
+            this.v.push(Math.round(this.d[i] / this.x[i] * 100))
+        }
+
+        this.interval = [];
+
+        for(let i = 0; i < Math.min(...this.scores.map(item => item.scores.length)); i++) {
+            this.interval.push({
+                lower: this.x[i] - 2.23 * this.d[i] / Math.sqrt(this.scores.length),
+                upper: this.x[i] + 2.23 * this.d[i] / Math.sqrt(this.scores.length),
+            })
+        }
+    }
+
     toResultObject() {
         return {
             s: {id: 'Si', values: this.s},
@@ -44,6 +81,11 @@ class Solver {
             itemRanks: this.itemRanks,
             ranksSum: this.ranksSum,
             resultRanks: this.resultRank,
+            Var: this.Var,
+            d: this.d,
+            sigma: this.sigma,
+            v: this.v,
+            interval: this.interval,
         }
     }
 }
